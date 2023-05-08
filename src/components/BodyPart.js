457a13cd3451b95bd668e5d82536ec90f4f4ac31
garyzhuo <gary_zhuo@hotmail.com>
2023-05-08 18:32:32 -0500

    Working on implementing the API to mount

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -3,7 +3,7 @@ import { Stack, Typography } from '@mui/material';
 
 import Icon from '../assets/icons/gym.png'
 
-const BodyPart = (item, setBodyPart, bodyPart) => {
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
     return (
         <Stack
             type='button'
@@ -19,9 +19,23 @@ const BodyPart = (item, setBodyPart, bodyPart) => {
                 cursor: 'pointer',
                 gap: '47px'
             }}
+            onClick={() => {
+                setBodyPart(item);
+                window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' })
+            }}
         >
             <img src={Icon} alt='dumbbell'
-                styles={{ width: '40px', height: '40px' }} />
+                style={{
+                    width: '40px', height: '40px'
+                }} />
+            <Typography
+                fontSize='24px'
+                fontWeight='bold'
+                color='#3A1212'
+                textTransform='capitalize'
+            >
+                {item}
+            </Typography>
         </Stack >
     )
 }
--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -31,9 +31,9 @@ const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
             {data.map((item) => (
                 <Box
                     key={item.id || item}
-                    itemId={item.id || item}
+                    itemID={item.id || item}
                     title={item.id || item}
-                    m="0 0px"
+                    m="0 40px"
                 >
                     <BodyPart item={item} bodyPart={bodyPart}
                         setBodyPart={setBodyPart}
