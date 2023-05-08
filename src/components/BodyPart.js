1f293951d875c6e88c8a23112d713d2f45f38357
garyzhuo <gary_zhuo@hotmail.com>
2023-05-08 18:15:06 -0500

    Fixed horizontal scroll functionaility

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -3,11 +3,26 @@ import { Stack, Typography } from '@mui/material';
 
 import Icon from '../assets/icons/gym.png'
 
-const BodyPart = () => {
+const BodyPart = (item, setBodyPart, bodyPart) => {
     return (
-        <Stack>
-            <img src={Icon} alt='dumbbell' styles={{ width: '40px', height: '40px' }} />
-        </Stack>
+        <Stack
+            type='button'
+            alignItems='center'
+            justifyContent='center'
+            className='bodyPart-card'
+            sx={{
+                borderTop: bodyPart === item ? '4px solid #000000' : '',
+                backgroundColor: '#fff',
+                borderBottomLeftRadius: '20px',
+                width: '270px',
+                height: '280px',
+                cursor: 'pointer',
+                gap: '47px'
+            }}
+        >
+            <img src={Icon} alt='dumbbell'
+                styles={{ width: '40px', height: '40px' }} />
+        </Stack >
     )
 }
 
