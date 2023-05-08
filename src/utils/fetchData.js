f4474cd6d81f43794c1372639adbb6a9d579c25d
garyzhuo <gary_zhuo@hotmail.com>
2023-05-08 13:23:28 -0500

    creating seperate branch

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
     method: 'GET',
     url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
     headers: {
-        'X-RapidAPI-Key': 'b75b1e97b5msh3f4ff0f7b2b59f4p10368bjsn59edd74e771b',
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
 };
