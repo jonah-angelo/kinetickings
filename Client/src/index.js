3b75f58471468b31851cd8d180911ec2ffc7a6da
jonah-angelo <jonah.angelo@gmail.com>
2023-05-10 15:16:16 -0700

    setting up redux

--- Client/src/index.js
+++ Client/src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const store = createStore(compose(applyMiddleware(thunk)));
 
 
 root.render(
--- Client/src/pages/PostPage.js
+++ Client/src/pages/PostPage.js
@@ -3,7 +3,7 @@ import { Container, AppBar, Typography, Grow, Grid  } from '@material-ui/core';
 
 import Posts from "../components/Posts/Posts";
 import Form from "../components/Form/Form";
-import useStyles from '../styles'
+import useStyles from './styles'
 
 
 const PostPage = () => {
