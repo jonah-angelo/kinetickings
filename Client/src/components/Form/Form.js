4d885799ba8814f1274568c10a6f73278db3193d
jonah-angelo <jonah.angelo@gmail.com>
2023-05-10 15:50:14 -0700

    working on form page

--- Client/src/components/Form/Form.js
+++ Client/src/components/Form/Form.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+
 import useStyles from './styles'
+
 const Form = () => {
     const classes = useStyles();
+    const [postData, setPostData] = useState({
+        creator: '',
+        title: '',
+        message: '',
+        tags: '',
+        selectedFile: ''
+    })
+
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <h1>Form</h1>
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+                <Typography variant="h6">Creating a Post</Typography>
+                <TextField name="creator" variant="outlined" label="Creator" fullWidthvalue={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
+                <TextField name="title" variant="outlined" label="Title" fullWidthvalue={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
+                <TextField name="message" variant="outlined" label="Message" fullWidthvalue={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
+                <TextField name="tags" variant="outlined" label="Tags" fullWidthvalue={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
+            </form>
+        </Paper>
     )
 }
 
--- Client/src/index.js
+++ Client/src/index.js
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from "redux";
+import { legacy_createStore as createStore, applyMiddleware, compose } from "redux";
 import thunk from 'redux-thunk';
 
 import reducers from './reducers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const store = createStore(compose(applyMiddleware(thunk)));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 )
